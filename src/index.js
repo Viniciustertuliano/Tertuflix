75be951c9a5825936556b1a61771cc4debf71464
vinicius Tertuliano da silva <viniciustertulianodasilva@hotmail.com>
2020-07-30 07:23:54 -0300

    Route configuration with 'react-router-dom'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+
+//Desafio de gerar algo legal no 404
+const Pagina404 = () => (<div>Pagina404, Pagina não encontrada </div>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter >
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo}  />
+      <Route path="/cadastro/categoria" component={CadastroCategoria}  />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
+  
   document.getElementById('root')
 );
