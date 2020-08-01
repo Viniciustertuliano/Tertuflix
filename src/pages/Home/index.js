fc99b27f8decce9bb9d2debcdf4512b591b86a0d
vinicius Tertuliano da silva <viniciustertulianodasilva@hotmail.com>
2020-07-31 23:35:13 -0300

    Removing component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,49 +3,43 @@ import Menu from '../../components/Menu';
 import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-import CarouselLivro from '../../components/CarouselLivro';
 import Footer from '../../components/Footer';
-
 
 function Home() {
   return (
-    <div style={{ background: "#141414" }}>
+    <div style={{ background: '#141414' }}>
       <Menu />
-      <BannerMain 
-      videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-      url={dadosIniciais.categorias[0].videos[0].url}
-      videoDescription={"Desafio: 10 projetos rápidos para treinar Programação e conseguir um Emprego"}
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription="Desafio: 10 projetos rápidos para treinar Programação e conseguir um Emprego"
       />
 
-      <Carousel 
-      category={dadosIniciais.categorias[0]}
+      <Carousel
+        category={dadosIniciais.categorias[0]}
       />
 
-      <Carousel 
-      category={dadosIniciais.categorias[1]}
+      <Carousel
+        category={dadosIniciais.categorias[1]}
       />
 
-      <Carousel 
-      category={dadosIniciais.categorias[2]}
+      <Carousel
+        category={dadosIniciais.categorias[2]}
       />
 
-      <Carousel 
-      category={dadosIniciais.categorias[3]}
+      <Carousel
+        category={dadosIniciais.categorias[3]}
       />
 
-      <CarouselLivro
-      category={dadosIniciais.categorias[4]}
+      <Carousel
+        category={dadosIniciais.categorias[5]}
       />
 
-      <Carousel 
-      category={dadosIniciais.categorias[5]}
-      />
-
-      <Carousel 
-      category={dadosIniciais.categorias[6]}
+      <Carousel
+        category={dadosIniciais.categorias[6]}
       />
       <Footer />
-      
+
     </div>
   );
 }
